59a4f2cf13359c4342815ca4270c44502ba79b21
Igor <igcarvalhaes@hotmail.com>
2023-04-10 17:14:21 -0300

    Add compare function between rock, paper & scissors

--- script.js
+++ script.js
@@ -4,10 +4,10 @@
 //     return (Math.floor(Math.random() * choice.length));
 // }
 
+
 function getComputerChoice () {
     const rockPaperScissors = ["Rock", "Paper", "Scissors"];
     const choice = (Math.floor(Math.random() * rockPaperScissors.length));
-    
     if (choice == 0) {
         return "Rock";
     } else if (choice == 1) {
@@ -17,5 +17,45 @@ function getComputerChoice () {
     }
 }
 
+function playRound (playerSelection, computerSelection) {
+    const player = playerSelection.toLowerCase();
+    const computer = computerSelection.toLowerCase();
 
-console.log(getComputerChoice());   
+   switch(player) {
+    case "rock":
+        if (computer === "rock"){
+            return "Tied!";
+        } else if (computer === "paper") {
+            return "You lost! Paper beats Rock!";
+        } else {
+            return "You won! Rock beats Scissors!";
+        }
+    break;
+    case "paper":
+        if (computer === "rock") {
+            return "You won! Paper beats Rock!";
+        } else if (computer === "paper") {
+            return "Tied!";
+        } else {
+            return "You lost! Scissors beats Paper!";
+        }
+    break;
+    case "scissors":
+        if(computer === "rock"){
+            return "You lost! Rock beats Scissors!";
+        } else if (computer === "paper"){
+            return "You won! Scissors beats Paper!";
+        } else {
+            return "Tied!";
+        }
+    break;
+    default:
+        return "Invalid input!";    
+   }
+}
+
+
+const playerSelection = "scissors";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
